8c2999a7fbc2d8dec528989bd088ecd6779fc69a
Mariia-cioccolatini <107420122+Mariia-cioccolatini@users.noreply.github.com>
2022-09-19 20:02:57 +0200

    key

--- src/Api.js
+++ src/Api.js
@@ -1,4 +1,4 @@
 export default class Api {
   static url = "https://api.openweathermap.org";
-  static key = "5f472b7acba333cd8a035ea85a0d4d4c";
+  static key = "44c2d281c7701ddf7d1ccc23fa0b2ff9";
 }
